143634aaf218f210f91c92e3e448ed354c768d0f
marcosOrdieres <udthades@gmail.com>
2021-09-02 17:01:28 +0200

    Update StartScreen.translations.ts

--- src/Pages/Start/StartScreen.translations.ts
+++ src/Pages/Start/StartScreen.translations.ts
@@ -7,8 +7,8 @@ export const useStartTranslations = makeTranslations({
     clickPlus: 'Click on the plus in order to get image recommendations',
   },
   de: {
-    mainTitle: 'Bildgenehmigungsantrag',
-    approvedImages: 'Genehmigte Bilder',
-    clickPlus: 'Klicken Sie auf das Plus, um Bildempfehlungen zu erhalten',
+    mainTitle: 'Image Approval Application',
+    approvedImages: 'Approved Images',
+    clickPlus: 'Click on the plus in order to get image recommendations',
   },
 });
